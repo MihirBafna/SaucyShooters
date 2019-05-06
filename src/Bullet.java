7634abb1115e7a65a6e782a2a862a3cc8740acc1
bsmider <bsmider18@gmail.com>
2019-05-06 08:20:37 -0700

    updated

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,37 +1,42 @@
+
 public class Bullet {
 
-    private double initX;
-    private double initY;
-    private double x;
-    private double y;
-    private double range;
-    private double BXVel;
-    private double BYVel;
-    private double size;
+	private double initX;
+	private double initY;
+	private double x;
+	private double y;
+	private double range;
+	private double rangeFallOff;
+	private double damage;
+	private double damageFallOff;
+	private double velX;
+	private double velY;
+	private double size;
 
-    public Bullet(double initX, double initY, double range, double BXVel, double BYVel, double size) {
-        this.initX = initX;
-        this.initY = initY;
-        x = initX;
-        y = initY;
-        this.range = range;
-        this.BXVel = BXVel;
-        this.BYVel =BYVel;
-        this.size = size;
-    }
+	public Bullet(double initX, double initY, double range, double rangeFallOff, double damage, double damageFallOff,
+			double velX, double velY, double size) {
+		this.initX = initX;
+		this.initY = initY;
+		x = initX;
+		y = initY;
+		this.range = range;
+		this.velX = velX;
+		this.velY = velY;
+		this.size = size;
+	}
 
-    public void travel() {
-        x += BXVel;
-        y += BYVel;
-    }
+	public void travel() {
+		x += velX;
+		y += velY;
+	}
 
-    public boolean expire() {
-        // pythagorean theorem to check hypotenuse
-        double squareAB = (Math.pow(x - initX, 2) + Math.pow(y - initY, 2));
-        if (Math.sqrt(squareAB) >= range) {
-            return true;
-        }
-        return false;
-    }
+	public boolean expire() {
+		// pythagorean theorem to check hypotenuse
+		double squareAB = (double) (Math.pow(x - initX, 2) + Math.pow(y - initY, 2));
+		if (Math.sqrt(squareAB) >= range) {
+			return true;
+		}
+		return false;
+	}
 
 }
